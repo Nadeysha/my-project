58b8fae5ff7bc40c752080fce8deabdece4e92f9
Novikova Nadezhda <nadeysha23@gmail.com>
2021-02-01 17:31:12 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let answers = [];
+
+/* answers[0] = prompt( "Hello, how are you?", "");
+
+
+document.write(answers); */
+
+
+const user = prompt("What is your name?", "");
+
+alert(`Hello, ${user}`);
