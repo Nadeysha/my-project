e64921197ba52e3c8c50e2bf3d0893f8756b5c5d
Novikova Nadezhda <nadeysha23@gmail.com>
2021-02-05 23:10:18 +0400

    practica2

--- js/script.js
+++ js/script.js
@@ -13,13 +13,35 @@ let personalMovieDB = {
     privat: false
 };
 
-let movies1 = prompt("Один из последних просмотренных фильмов?", ""),
-    movies2 = +prompt("На солько оцените его?", ""),
-    movies3 = prompt("Один из последних просмотренных фильмов?", ""),
-    movies4 = +prompt("На солько оцените его?", "");
 
 
-personalMovieDB.movies[movies1] = movies2;
-personalMovieDB.movies[movies3] = movies4;
+for (let i = 1; i < 2; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = +prompt("На солько оцените его?", "");
+        if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB[a] = b;
+        console.log('Done');
+        } else {
+            console.log('error');
+            i--
+        };
+
+};
+
+/* i-- (возврат на шаг назад) проставили для того чтобы если пользователь ввел первый
+ раз не соотетствующее условиям значение, то оба вопроса еще раз спросились в любом случае, 
+ так как по условию пользователь должен ответить 2 раза и 2 раза записаться данные */
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+};
 
 console.log(personalMovieDB);
